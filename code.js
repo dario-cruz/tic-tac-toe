4fca167053dce23b2a862a07732a895731a3ebed
Dario Cruz <rio2012@gmail.com>
2022-11-07 16:17:22 -0500

    Add const for dom elements in code.js

---
+++ code.js
@@ -0,0 +1,12 @@
+// Defining parts of the gameboard.
+const gameBoard = document.getElementById('gameboard')
+const gameSquareTL = document.getElementById('topleft')
+const gameSquareML = document.getElementById('midleft')
+const gameSquareBL = document.getElementById('bottomleft')
+const gameSquareTM = document.getElementById('topmid')
+const gameSquareCM = document.getElementById('centermid')
+const gameSquareBM = document.getElementById('bottommid')
+const gameSquareTR = document.getElementById('topright')
+const gameSquareMR = document.getElementById('midright')
+const gameSquareBR = document.getElementById('bottomright')
+
