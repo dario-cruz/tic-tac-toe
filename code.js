01c7429246c061d9ca82402efd058244f68390c9
Dario Cruz <rio2012@gmail.com>
2022-11-09 06:31:59 -0500

    Organize elems of code.

--- code.js
+++ code.js
@@ -7,21 +7,8 @@ const dario = gamePlayer("Dario", "x")
 console.log(dario)
 
 // Create gamebord object. 
+
 const gameBoard = (() => {
-    // Defining the gameboard positions in object. 
-    // When the player or computer picks a position,
-    // This will update.
-    // Then an evaluation will take place.
-    // Once the game is won and event will be triggered to mod the dom.
-    // 
-    const gameBoardPositions = {
-       rowOne:[0,0,0],
-       rowTwo:[0,0,0],
-       rowThree:[0,0,0]
-    }
-
-    
-
     // Defining parts of the gameboard.
     const gameBoardDomElem = document.getElementById('gameboard')
     const gameSquareTL = document.getElementById('topleft')
@@ -34,9 +21,22 @@ const gameBoard = (() => {
     const gameSquareMR = document.getElementById('midright')
     const gameSquareBR = document.getElementById('bottomright')
     
+    
+    // Defining the gameboard positions in object. 
+    // When the player or computer picks a position,
+    // This will update.
+    // Then an evaluation will take place.
+    // Once the game is won and event will be triggered to mod the dom.
+
+    const gameBoardPositions = {
+       rowOne:[0,0,0],
+       rowTwo:[0,0,0],
+       rowThree:[0,0,0]
+    }
+
     const gameEval = () => {
         if (gameBoardPositions.rowOne == [1,1,1]) {
             console.log('Player One Wins!')
         }
     }
-})
+})();
